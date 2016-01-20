d967655699e56c75444a03b3ff4728f4bda2c1b8
Jordan Meier and Taylor Pokoj <pokojt@gmail.com>
2016-01-20 10:54:10 -0800

    Add functionality to second test by moving the first letter to the end of the word if it starts with a consonant and adding ay

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,23 @@
 var pigLatin = function(message){
   var vowel = ['a', 'e', 'i', 'o', 'u'];
   var addAy = "ay";
+  var firstLetter = message.slice(0,1);
+  var restWord = message.slice(1, message.length);
+  var firstTwoLetters = message.slice(0,2);
+
   for (var i=0; i < vowel.length; i++)
     if (message[0] === vowel[i]) {
-    return (message + addAy);
+      return (message + addAy);
   };
-};
+
+  if (message[0] !== vowel[i]) {
+    return (restWord + firstLetter + addAy);
+
+//   } else if (message[0,2] !== vowel[i]) {
+//     return (restWord + firstTwoLetters + addAy);
+//
+//   };
+// };
 
 
 //
@@ -20,28 +32,3 @@ var pigLatin = function(message){
 //     event.preventDefault();
 //   });
 // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// var addAy = function (message) //split the message by spaces
-//   var array = [];
-//   var string = message.split("");
-//
-//   for (i=0; i <= message.)
--- spec/specs.js
+++ spec/specs.js
@@ -2,37 +2,17 @@ describe ('pigLatin', function () {
   it("adds 'ay' to the end of a word starting with a vowel", function () {
     expect(pigLatin("island")).to.equal("islanday");
   });
-});
 
+  it("moves first letter to end of word if it starts with one consonant and adds 'ay'", function() {
+    expect(pigLatin("coffee")).to.equal("offeecay");
+  });
 
-//   it("moves first letter to end of word if it starts with one consonant", function() {
-//     expect(oneConsonant("coffee")).to.equal("offeec");
+//   it("moves first two letters to end of word if it starts with two consonant and adds 'ay'", function() {
+//     expect(pigLatin("pretty")).to.equal("ettypray");
 //   });
 // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// describe('addAy', function() {
-//   it("adds 'ay' to the end of each word", function () {
-//     expect(addAy("and")).to.equal("anday");
-//   });
-// });
+//
+//
+//
+//
+//
