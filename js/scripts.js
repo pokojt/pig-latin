b0af78cf99ce67534b2dad5b9c96ef49e0dabfeb
Jordan Meier and Taylor Pokoj <pokojt@gmail.com>
2016-01-20 10:13:42 -0800

    Add functionality to first test, adding ay to end of words starting with vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,47 @@
-var pigLatin = function(message){
 
+
+
+var pigLatin = function(message){
+  var vowel = ['a', 'e', 'i', 'o', 'u'];
+  var addAy = "ay";
+  for (var i=0; i < vowel.length; i++)
+    if (message[0] === vowel[i]) {
+    return (message + addAy);
+  };
 };
 
-$(document).ready(function() {
-  $("form").submit(function(event) {
+
+//
+// $(document).ready(function() {
+//   $("form").submit(function(event) {
+//
+//
+//     $("#result").show();
+//     event.preventDefault();
+//   });
+// });
 
 
-    $("#result").show();
-    event.preventDefault();
-  });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var addAy = function (message) //split the message by spaces
+//   var array = [];
+//   var string = message.split("");
+//
+//   for (i=0; i <= message.)
---
+++ spec/specs.js
@@ -0,0 +1,38 @@
+describe ('pigLatin', function () {
+  it("adds 'ay' to the end of a word starting with a vowel", function () {
+    expect(pigLatin("island")).to.equal("islanday");
+  });
+});
+
+
+//   it("moves first letter to end of word if it starts with one consonant", function() {
+//     expect(oneConsonant("coffee")).to.equal("offeec");
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('addAy', function() {
+//   it("adds 'ay' to the end of each word", function () {
+//     expect(addAy("and")).to.equal("anday");
+//   });
+// });
