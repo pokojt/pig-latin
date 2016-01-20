48f8856215341861db34b144525432742dad6f56
Jordan Meier and Taylor Pokoj <pokojt@gmail.com>
2016-01-20 14:21:36 -0800

    Completely refactor code. Basically starting over with Test 1 functionality for adding ay to the end of a word that starts with a vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,79 @@
 
+var vowel = ['a', 'e', 'i', 'o', 'u'];
+var addAy = "ay";
 
 
-var pigLatin = function(message){
-  var vowel = ['a', 'e', 'i', 'o', 'u'];
-  var addAy = "ay";
-  var firstLetter = message.slice(0,1);
-  var restWord = message.slice(1);
-  var restWord2 = message.slice(2);
-  var firstTwoLetters = message.slice(0,2);
-
+var findVowel = function(message) {
   for (var i=0; i < vowel.length; i++){
     if (message[0] === vowel[i]) {
       return message + addAy;
-    }
-  }
-
-  for (var j=0; j < vowel.length; j++){
-      if (message[0] !== vowel[j]) {
-        if (message[1] !== vowel[j]) {
-          return restWord2 + firstTwoLetters + addAy;
-        }
-        return restWord + firstLetter + addAy;
-    }
-    // else if ((message.charAt(0) !== vowel[j]) && (message.charAt(1) !== vowel[j])) {
-    //   return restWord + firstTwoLetters + addAy;
-    // }
-  }
+    };
+  };
 };
 
+// var findConsonant = function(message) {
+//   for (var i=0; i < message.length; i++) {
+//
+//     for (var j=0; j < vowel.length; j++) {
+//       if (message.charAt([i]) !== vowel[j]) {
+//       }
+//       else {
+//         return message.slice([i]) + message.slice(0,[(i)]) + addAy;
+//       };
+//     };
+//   };
+// };
 
+
+
+//EVERYTHING BELOW THIS IS CODE FROM EARLIER THAT WE COULDN'T
+//GET TO TEST, KEEPING FOR REFERENCE FOR NOW.
+
+
+
+
+
+
+// var firstLetter = message.slice(0,1);
+// var restWord = message.slice(1, message.length);
+// var restWord2 = message.slice(2, message.length);
+// var firstTwoLetters = message.slice(0,2);
+
+
+
+
+
+
+// var pigLatin = function(message){
+//   var vowel = ['a', 'e', 'i', 'o', 'u'];
+//   var addAy = "ay";
+//   var firstLetter = message.slice(0,1);
+//   var restWord = message.slice(1, message.length);
+//   var restWord2 = message.slice(2, message.length);
+//   var firstTwoLetters = message.slice(0,2);
+//
+//   for (var i=0; i < vowel.length; i++){
+//     if (message[0] === vowel[i]) {
+//       return message + addAy;
+//     }
+//   }
+//   if ((message[0] !== vowel[i]) && )) {
+//     return restWord + firstLetter + addAy;
+//   }
+//
+//
+//
+//
+//       if (message[1] !== vowel[j]) {
+//         return restWord + firstLetter + addAy;
+//     } else if ((message[0] !== vowel[j]) && (message[1] !== vowel[j])) {
+//       return restWord2 + firstTwoLetters + addAy;
+//
+//
+//   }
+// };
+//
+//
 //
 // $(document).ready(function() {
 //   $("form").submit(function(event) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,11 @@
-describe ('pigLatin', function () {
+describe ('findVowel', function () {
   it("adds 'ay' to the end of a word starting with a vowel", function () {
-    expect(pigLatin("island")).to.equal("islanday");
-  });
-
-  it("moves first letter to end of word if it starts with one consonant and adds 'ay'", function() {
-    expect(pigLatin("carousel")).to.equal("arouselcay");
-  });
-
-  it("moves first two letters to end of word if it starts with two consonant and adds 'ay'", function() {
-    expect(pigLatin("pretty")).to.equal("ettypray");
+    expect(findVowel("island")).to.equal("islanday");
   });
 });
+
+// describe ('findConsonant', function () {
+//   it("moves all consonants before the first vowel to end of word and adds 'ay'", function() {
+//     expect(findConsonant("coffee")).to.equal("offeecay");
+//   });
+// });
